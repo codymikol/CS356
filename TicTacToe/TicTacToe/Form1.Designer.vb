9a06e771269de48c25b055ca69304f385e6fc42a
pranshugupta123 <pranshu.gupta@desales.edu>
2018-01-09 12:58:16 -0500

    TicTacToe files

---
+++ TicTacToe/TicTacToe/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn1.Image = CType(resources.GetObject("btn1.Image"), System.Drawing.Image)
+        Me.btn1.Location = New System.Drawing.Point(13, 23)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(77, 75)
+        Me.btn1.TabIndex = 0
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn2.Image = CType(resources.GetObject("btn2.Image"), System.Drawing.Image)
+        Me.btn2.Location = New System.Drawing.Point(96, 23)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(77, 75)
+        Me.btn2.TabIndex = 1
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn3.Image = CType(resources.GetObject("btn3.Image"), System.Drawing.Image)
+        Me.btn3.Location = New System.Drawing.Point(179, 23)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(77, 75)
+        Me.btn3.TabIndex = 2
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn4.Image = CType(resources.GetObject("btn4.Image"), System.Drawing.Image)
+        Me.btn4.Location = New System.Drawing.Point(12, 104)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(77, 75)
+        Me.btn4.TabIndex = 3
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn7.Image = CType(resources.GetObject("btn7.Image"), System.Drawing.Image)
+        Me.btn7.Location = New System.Drawing.Point(13, 185)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(77, 75)
+        Me.btn7.TabIndex = 4
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn5.Image = CType(resources.GetObject("btn5.Image"), System.Drawing.Image)
+        Me.btn5.Location = New System.Drawing.Point(96, 104)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(77, 75)
+        Me.btn5.TabIndex = 5
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn8.Image = CType(resources.GetObject("btn8.Image"), System.Drawing.Image)
+        Me.btn8.Location = New System.Drawing.Point(96, 185)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(77, 75)
+        Me.btn8.TabIndex = 6
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn6.Image = CType(resources.GetObject("btn6.Image"), System.Drawing.Image)
+        Me.btn6.Location = New System.Drawing.Point(179, 104)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(77, 75)
+        Me.btn6.TabIndex = 7
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn9.Image = CType(resources.GetObject("btn9.Image"), System.Drawing.Image)
+        Me.btn9.Location = New System.Drawing.Point(179, 185)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(77, 75)
+        Me.btn9.TabIndex = 8
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(272, 277)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+
+End Class
---
+++ TicTacToe/TicTacToe/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If btn1.Enabled = True Then
+            btn1.Text = "O"
+            btn1.Enabled = False
+        End If
+        If btn2.Enabled = True Then
+            btn2.Text = "O"
+            btn2.Enabled = False
+        End If
+        If btn3.Enabled = True Then
+            btn3.Text = "O"
+            btn3.Enabled = False
+        End If
+        If btn4.Enabled = True Then
+            btn4.Text = "O"
+            btn4.Enabled = False
+        End If
+        If btn5.Enabled = True Then
+            btn5.Text = "O"
+            btn5.Enabled = False
+        End If
+        If btn6.Enabled = True Then
+            btn6.Text = "O"
+            btn6.Enabled = False
+        End If
+        If btn7.Enabled = True Then
+            btn7.Text = "O"
+            btn7.Enabled = False
+        End If
+        If btn8.Enabled = True Then
+            btn8.Text = "O"
+            btn8.Enabled = False
+        End If
+        If btn9.Enabled = True Then
+            btn9.Text = "O"
+            btn9.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+
+    End Sub
+End Class
